07a17edfb46efb57c4676db2f3c472f7b71c5453
Samuel Tai Meng Yao <samuel.tai@grabtaxi.com>
2024-05-18 23:12:50 +0800

    Added in simple html pages for author and books

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,7 +1,10 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """View function for home page of site."""
@@ -25,3 +28,36 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'catalog/index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'   # your own name for the list as a template variable
+    template_name = 'book_list.html'  # Specify your own template name/location
+
+    paginate_by = 2
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset()
+        # return Book.objects.filter(title__icontains='success')[:5] # Get 5 books containing the title success
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+    
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    context_object_name = 'author_list'
+    template_name = 'author_list.html'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset()
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    
